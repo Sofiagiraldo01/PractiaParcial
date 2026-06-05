76567450b9819f112405a4312a4db5085ded8628
Sofiagiraldo01 <Sgiraldo_34@cue.edu.co>
2026-06-05 15:23:50 -0500

    parkingSteps y RunCucumberTest

--- src/test/java/com/parkinguv/bdd/ParkingSteps.java
+++ src/test/java/com/parkinguv/bdd/ParkingSteps.java
@@ -1,4 +1,39 @@
 package com.parkinguv.bdd;
 
+import com.parkinguv.ParkingCalculator;
+import io.cucumber.java.en.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ParkingSteps {
+
+    private int minutes;
+    private boolean vip;
+    private double result;
+
+    private final ParkingCalculator calculator =
+            new ParkingCalculator();
+
+    @Given("a vehicle stayed for {int} minutes")
+    public void vehicleStayed(int minutes) {
+        this.minutes = minutes;
+    }
+
+    @Given("the customer is VIP")
+    public void vipCustomer() {
+        this.vip = true;
+    }
+
+    @When("the parking fee is calculated")
+    public void calculateFee() {
+        result = calculator.calculateFee(
+                minutes,
+                vip
+        );
+    }
+
+    @Then("the total amount should be {double}")
+    public void verify(double expected) {
+        assertEquals(expected, result);
+    }
 }
--- src/test/java/com/parkinguv/bdd/RunCucumberTest.java
+++ src/test/java/com/parkinguv/bdd/RunCucumberTest.java
@@ -1,6 +1,13 @@
 package com.parkinguv.bdd;
 
+import org.junit.platform.suite.api.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(
+        key = "cucumber.plugin",
+        value = "pretty"
+)
 public class RunCucumberTest {
-    public static class ParkingSteps {
-    }
 }
